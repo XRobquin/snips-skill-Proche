20fc5d61ce4e485b1e7c372f45e44146a7e974dc
XRobquin <xrobquin@enseirb-matmeca.fr>
2019-09-24 16:54:00 +0200

    Update and rename action-discussion.py to action-proche.py

--- action-proche.py
+++ action-proche.py
@@ -12,10 +12,7 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
 	
-	liste_reponses = ["Je suis ravie, comment etait "]
-	sentence = liste_reponses[random.randint(0,len(liste_reponses)-1)]
-	lieu = intent_message.slots.Lieu.first().value
-	sentence += lieu
+	sentence = "Je suis ravie"
 	hermes.publish_end_session(intent_message.session_id, sentence)
 
 
